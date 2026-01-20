f66cea7e0986c68f0c8827d38c653b73b1b1688a
jaa <farezaahmadk@gmail.com>
2026-01-20 10:33:40 +0800

    Add train classifier code
    This commit adding a train classifier code that give 12 training process

--- code/main.py
+++ code/main.py
@@ -5,6 +5,8 @@ from skimage.io import imread
 from skimage.transform import resize
 import numpy as np
 from sklearn.model_selection import train_test_split    
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 # prepare data
 current_dir = Path(__file__).parent
@@ -28,5 +30,12 @@ labels = np.asarray(labels)
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True,stratify=labels)
 
 # train classifier
+classifier = SVC()
+
+parameters = [{'gamma': [0.01, 0.001, 0.0001],'C': [1, 10 , 100, 1000]}]
+
+grid_search = GridSearchCV(classifier, parameters)
+
+grid_search.fit(x_train, y_train)
 
 # test performance
